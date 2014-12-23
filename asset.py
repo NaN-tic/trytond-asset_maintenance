b19e51fa1508abc1b4bbaaa700716f0648878b9c
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-12-23 12:50:34 +0100

    Add option to show all maintenances and relate from asset and Add maintenance interval to asset

--- asset.py
+++ asset.py
@@ -1,7 +1,10 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
+import datetime
+from dateutil.relativedelta import relativedelta
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
 
 __all__ = ['Category', 'Asset', 'Maintenance']
 __metaclass__ = PoolMeta
@@ -15,8 +18,44 @@ class Category(ModelSQL, ModelView):
 
 class Asset:
     __name__ = 'asset'
+    maintenance_interval = fields.Selection([
+            ('none', 'Without Maintenance'),
+            ('day', '01 - Day'),
+            ('week', '02 - Week'),
+            ('month', '03 - Month'),
+            ('year', '04 - Year'),
+        ], 'Maintenance Interval', required=True)
+    maintenance_interval_number = fields.Integer('Maintenance Interval Count',
+        states={
+            'required': Eval('maintenance_interval') != 'none',
+            'invisible': Eval('maintenance_interval') == 'none',
+            },
+        depends=['maintenance_interval'])
     mantineances = fields.One2Many('asset.maintenance', 'asset',
         'Maintenances')
+
+    @staticmethod
+    def default_maintenance_interval():
+        return ''
+
+    def compute_next_maintenance_date(self, date):
+        """
+        Compute new maintendance date for the asset based on date
+        :param date: a date: the date of the last maintenance
+        """
+        date += datetime.timedelta(days=1)
+
+        interval_number = self.maintenance_interval_number
+        if self.maintenance_interval == 'day':
+            interval_date = relativedelta(days=interval_number)
+        elif self.maintenance_interval == 'week':
+            interval_date = relativedelta(weeks=interval_number)
+        elif self.maintenance_interval == 'month':
+            interval_date = relativedelta(months=interval_number)
+        else:  # year
+            interval_date = relativedelta(years=interval_number)
+
+        return date + interval_date - relativedelta(days=1)
 
 
 class Maintenance(ModelSQL, ModelView):
@@ -32,7 +71,7 @@ class Maintenance(ModelSQL, ModelView):
     date_done = fields.Date('Done Date')
     date_next = fields.Date('Next Maintenance')
     note = fields.Text('Note')
-    party = fields.Many2One('party.party', 'Workshop')
+    party = fields.Many2One('party.party', 'Party')
     reference = fields.Reference('Reference', selection='get_reference',
         select=True)
 
@@ -51,18 +90,9 @@ class Maintenance(ModelSQL, ModelView):
                 ])
         return [(None, '')] + [(m.model, m.name) for m in models]
 
-
-class Vehicle(ModelSQL, ModelView):
-    'Vehicle'
-    __name__ = 'asset.vehicle'
-    asset = fields.Many2One('asset', 'Asset', required=True)
-    driver = fields.Many2One('company.employee', 'Driver',
-        help='The driver that normally drives this vehicle')
-    technical_description = fields.Text('Technical Description')
-
-    def get_rec_name(self, name):
-        return self.asset.name
-
-    @classmethod
-    def search_rec_name(cls, name, clause):
-        return [('asset.name',) + tuple(clause[1:])]
+    @fields.depends('date_planned', 'date_start', 'date_done', 'asset')
+    def on_change_with_date_next(self):
+        if self.asset and self.asset.maintenance_interval_number:
+            date = self.date_done or self.date_start or self.date_planned
+            if date:
+                return self.asset.compute_next_maintenance_date(date)
