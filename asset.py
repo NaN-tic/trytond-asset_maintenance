3cc26cb330b2684e7fdda691f95f5eac01f78126
Raimon Esteve <raimon@nan-tic.com>
2016-09-15 13:44:43 +0200

    Remove asset domain by company because there is a rule

--- asset.py
+++ asset.py
@@ -2,7 +2,6 @@
 #copyright notices and license terms.
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval
 
 __all__ = ['Category', 'Asset', 'Maintenance']
 __metaclass__ = PoolMeta
@@ -24,10 +23,8 @@ class Maintenance(ModelSQL, ModelView):
     'Asset Maintenance'
     __name__ = 'asset.maintenance'
 
-    asset = fields.Many2One('asset', 'Asset', required=True, ondelete='CASCADE',
-        domain=[
-            ('company', '=', Eval('context', {}).get('company', -1)),
-            ])
+    asset = fields.Many2One('asset', 'Asset', required=True,
+        ondelete='CASCADE')
     category = fields.Many2One('asset.maintenance.category', 'Category',
         required=True)
     date_planned = fields.Date('Planned Date')
