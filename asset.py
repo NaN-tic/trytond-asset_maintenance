d1ba05c2bcd95a9ba4e5b8ab28740b21a5a00c56
Raimon Esteve <raimon@nan-tic.com>
2016-09-15 12:55:11 +0200

    Add rule by company

--- asset.py
+++ asset.py
@@ -2,6 +2,7 @@
 #copyright notices and license terms.
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
 
 __all__ = ['Category', 'Asset', 'Maintenance']
 __metaclass__ = PoolMeta
@@ -23,8 +24,10 @@ class Maintenance(ModelSQL, ModelView):
     'Asset Maintenance'
     __name__ = 'asset.maintenance'
 
-    asset = fields.Many2One('asset', 'Asset', required=True,
-        ondelete='CASCADE')
+    asset = fields.Many2One('asset', 'Asset', required=True, ondelete='CASCADE',
+        domain=[
+            ('company', '=', Eval('context', {}).get('company', -1)),
+            ])
     category = fields.Many2One('asset.maintenance.category', 'Category',
         required=True)
     date_planned = fields.Date('Planned Date')
@@ -35,6 +38,8 @@ class Maintenance(ModelSQL, ModelView):
     party = fields.Many2One('party.party', 'Party')
     reference = fields.Reference('Reference', selection='get_reference',
         select=True)
+    company = fields.Function(fields.Many2One('company.company', 'Company'),
+        'on_change_with_company', searcher='search_company')
 
     @staticmethod
     def _get_reference():
@@ -50,3 +55,12 @@ class Maintenance(ModelSQL, ModelView):
                 ('model', 'in', models),
                 ])
         return [(None, '')] + [(m.model, m.name) for m in models]
+
+    @fields.depends('asset')
+    def on_change_with_company(self, name=None):
+        if self.asset and self.asset.company:
+            return self.asset.company.id
+
+    @classmethod
+    def search_company(cls, name, clause):
+        return [('asset.%s' % name,) + tuple(clause[1:])]
