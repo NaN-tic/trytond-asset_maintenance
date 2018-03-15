943abc89f02a104a3fa9c0faa133df380c8fb651
Albert Cervera i Areny <albert@nan-tic.com>
2018-03-15 12:56:42 +0100

    Adapt to 4.0 and other improvements.

--- asset.py
+++ asset.py
@@ -1,21 +1,22 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
 from trytond.model import ModelSQL, ModelView, fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 
 __all__ = ['Category', 'Asset', 'Maintenance']
-__metaclass__ = PoolMeta
 
 
 class Category(ModelSQL, ModelView):
     'Asset Maintenance Category'
     __name__ = 'asset.maintenance.category'
-    name = fields.Char('Name', required=True, translate=True)
+    name = fields.Char('Name', required=True)
 
 
 class Asset:
     __name__ = 'asset'
-    mantineances = fields.One2Many('asset.maintenance', 'asset',
+    __metaclass__ = PoolMeta
+    maintenances = fields.One2Many('asset.maintenance', 'asset',
         'Maintenances')
 
 
@@ -27,26 +28,23 @@ class Maintenance(ModelSQL, ModelView):
         ondelete='CASCADE')
     category = fields.Many2One('asset.maintenance.category', 'Category',
         required=True)
-    date_planned = fields.Date('Planned Date')
-    date_start = fields.Date('Start Date')
-    date_done = fields.Date('Done Date')
-    date_next = fields.Date('Next Maintenance')
+    date_planned = fields.Date('Planned Date', states={
+            'required': Eval('state') == 'planned',
+            })
+    date_start = fields.Date('Start Date', states={
+            'required': Eval('state').in_(['in-progress', 'done']),
+            })
+    date_done = fields.Date('Done Date', states={
+            'required': Eval('state') == 'done',
+            })
     note = fields.Text('Note')
     party = fields.Many2One('party.party', 'Party')
-    reference = fields.Reference('Reference', selection='get_reference',
-        select=True)
+    state = fields.Selection([
+            ('planned', 'Planned'),
+            ('in-progress', 'In Progress'),
+            ('done', 'Done'),
+            ], 'State', required=True)
 
     @staticmethod
-    def _get_reference():
-        'Return list of Model names for origin Reference'
-        return []
-
-    @classmethod
-    def get_reference(cls):
-        pool = Pool()
-        IrModel = pool.get('ir.model')
-        models = cls._get_reference()
-        models = IrModel.search([
-                ('model', 'in', models),
-                ])
-        return [(None, '')] + [(m.model, m.name) for m in models]
+    def default_state():
+        return 'planned'
